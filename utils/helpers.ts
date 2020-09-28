4c7dc66d01408e412f78af46776d47684f295a17
iMykhailychenko <igor.c.m@ukr.net>
2020-09-28 21:22:03 +0300

    fix time in message

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,20 +2,28 @@ const addZeroToNumber = (value: string | number): string =>
   String(value).padStart(2, '0');
 
 const formateDate = (ms: number): string => {
-  const diferent: number = (Date.now() - ms) / 1000;
-  if (diferent < 60) return 'a few seconds ago'; // 60 - seconds
-  if (diferent > 60 && diferent < 3600)
-    return `${Math.ceil(diferent / 3600)} minuts ago`; // 3600 - seconds in hour
-  if (diferent > 3600 && diferent < 86400)
-    return `${Math.ceil(diferent / 86400)} hours ago`; // 86400 - seconds in a day
-  if (diferent < 86400) {
-    const date: Date = new Date(ms);
-    return `${addZeroToNumber(date.getDate())}.${addZeroToNumber(
-      date.getMonth()
-    )}.${date.getFullYear()}  ${addZeroToNumber(
-      date.getHours()
-    )}:${addZeroToNumber(date.getMinutes())}`;
-  }
+  const seconds: number = (Date.now() - ms) / 1000;
+  
+  // return 'a few seconds ago' if message was sent during last minut
+  if (seconds < 60) return 'a few seconds ago';
+
+  // return time in minuts if message was sent during last hour
+  const minuts: number = seconds / 60;
+  if (seconds > 60 && minuts < 60)
+    return `${Math.ceil(minuts)} minuts ago`;
+
+  // return time in hour if message was sent during last 24 hours
+  const hours: number = minuts / 24;
+  if (minuts > 60 && hours < 24)
+    return `${Math.ceil(hours)} hours ago`;
+
+  // else
+  const date: Date = new Date(ms);
+  return `${addZeroToNumber(date.getDate())}.${addZeroToNumber(
+    date.getMonth()
+  )}.${date.getFullYear()}  ${addZeroToNumber(
+    date.getHours()
+  )}:${addZeroToNumber(date.getMinutes())}`;
 };
 
 export default formateDate;
